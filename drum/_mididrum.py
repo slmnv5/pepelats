c2038c20be0871baaad016e634c05f63f22ac188
slmnv5 <slmnv5@gmail.com>
2022-11-19 01:02:30 -0500

    1

--- drum/_mididrum.py
+++ drum/_mididrum.py
@@ -3,7 +3,7 @@ import mido
 from drum._fakedrum import FakeDrum
 
 
-# noinspection PyMethodMayBeStatic
+# noinspection PyUnresolvedReferences
 class MidiDrum(FakeDrum):
     def __init__(self):
         self.__out_port = mido.open_output("pepelats_cloc")
