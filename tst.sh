d9b1cb07b0f226eaa34e9b9da82ebf2dd63a6c05
slmnv5 <slmnv5@gmail.com>
2022-12-31 16:32:17 -0500

    1

--- tst.sh
+++ tst.sh
@@ -11,17 +11,6 @@ export KBD_NOTES='"q": 12, "w": 13, "1":60, "2": 62, "3": 64, "4": 65'
 export MAX_LEN_SECONDS=60
 export SD_RATE=44100
 
-# Use this MIDI port as input
-export ENV_MIDI_IN_PORT='BlueBoard'
-# Use this MIDI port as clock output
-export ENV_MIDI_OUT_PORT='Sshpadnew'
-
-#check ALSA devices and use first one found
-export USB_AUDIO_NAMES='VALETON GP,USB Audio'
-
-export ENV_ROOT_DIR=$(dirname "$0")
-cd "$ENV_ROOT_DIR/src" || exit 1
-
 #killall -9 python
 export PYTHONPATH="${PYTHONPATH}:$HOME/mypi_music/src:$HOME/mypi_music"
 
