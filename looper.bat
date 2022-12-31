0c9520a32812ab5d85990bddd912496a3ab04c24
slmnv5 <slmnv5@gmail.com>
2022-12-31 16:56:45 -0500

    1

--- looper.bat
+++ looper.bat
@@ -1,22 +1,3 @@
-:: This script starts pepelats audio looper
-:: Options are:
-:: --debug or --info - level of logging
-:: --kbd - use keyboard for MIDI input (see KBD_NOTES)
-:: --count - count notes
-
-set "KBD_NOTES="1":60, "2": 62, "3": 64, "4": 65, "q": 12, "w": 13"
-
-::Looper parameters passed via env.
-set MAX_LEN_SECONDS=60
-set SD_RATE=44100
-
-::Use this MIDI port as input
-set ENV_MIDI_IN_PORT='BlueBoard'
-::Use this MIDI port as clock output
-set ENV_MIDI_OUT_PORT='Sshpadnew'
-
-::Check ALSA devices and use first one found
-set USB_AUDIO_NAMES='VALETON GP,USB Audio'
 
 set ENV_ROOT_DIR=%~dp0
 cd "%ENV_ROOT_DIR%\src" || goto stop
@@ -29,8 +10,10 @@ tasklist /v /fo csv | find "start_looper_cmd" && goto stop
 :loop
 title start_looper_cmd
 taskkill /F /IM python.exe
+%ENV_ROOT_DIR%\.saved_env.bat
 python.exe ./start_looper.py %*
 timeout 10
+
 goto loop
 
 title no_title
