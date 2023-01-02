6f3da89eaf0011159162f9a6b40145ca826c2b30
slmnv5 <slmnv5@gmail.com>
2023-01-02 15:14:46 -0500

    1

--- src/buffer/_wrapbuffer.py
+++ src/buffer/_wrapbuffer.py
@@ -97,13 +97,13 @@ class WrapBuffer:
             rec_len += trim_len
         # rec_len is multiple of trim_len: .. 1/8, 1/4, 1/2, 1, 2, 3, ...
         # align start with main loop's trim_len
-        offset: int = self.__start % trim_len
-        if offset < trim_len // 2:
-            self.__start -= offset
-        else:
-            self.__start += (trim_len - offset)
+        # offset: int = self.__start % trim_len
+        # if offset < trim_len // 2:
+        #    self.__start -= offset
+        # else:
+        #    self.__start += (trim_len - offset)
 
-        assert self.__start >= 0
+        # assert self.__start >= 0
 
         new_buff = make_zero_buffer(rec_len)
         play_sound_buff(self.__buff, new_buff, self.__start)
