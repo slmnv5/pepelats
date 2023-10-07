7129c03e68e9c913ea63b1fecfb86ba64795aefa
pi <pi@pepe1.com>
2023-10-07 17:52:11 +0100

    1

--- connect_bt.sh
+++ connect_bt.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+result=$(aconnect -l | grep "^client " | grep -i ssh)
+if [ -n "$result" ]; then echo "Connected to client $result"; exit 0; fi
+
 
 sudo bluetoothctl connect DC:86:D8:AF:D5:C5
+if [ $? -eq 0 ]; then exit 0; fi
+
+sudo bluetoothctl connect 9C:F4:8E:16:66:19 
+if [ $? -eq 0 ]; then exit 0; fi
+
+sudo bluetoothctl connect 58:E6:BA:67:24:64 
+if [ $? -eq 0 ]; then exit 0; fi
 
 
 
-
+exit 1
 
 
