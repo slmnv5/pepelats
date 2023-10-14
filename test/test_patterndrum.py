7022559c2fbdf68d561ec89c8afd26891562af79
slmnv5 <slmnv5@gmail.com>
2023-10-14 13:00:28 -0400

    1

--- test/test_patterndrum.py
+++ test/test_patterndrum.py
@@ -6,6 +6,7 @@ from utils.utilfactory import create_drum
 
 def test_1():
     dr = create_drum("PatternDrum")
+    dr.start_drum()
     dr.load_drum_config(None, 100_000)
     arr = make_zero_buffer(120_000)
     dr.play_drums(arr, 0)
