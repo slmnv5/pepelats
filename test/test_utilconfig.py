eb89d856092f971f9381d30cece21e7c9cc4a7b6
slmnv5 <slmnv5@gmail.com>
2024-05-27 20:12:34 -0400

    1

--- test/test_utilconfig.py
+++ test/test_utilconfig.py
@@ -2,7 +2,7 @@ from utils.utilconfig import load_ini_section, find_path, ConfigName
 
 
 def test_1():
-    dic = load_ini_section(find_path(ConfigName.main_ini), "AINFO")
+    dic = load_ini_section(find_path(ConfigName.main_ini), "AUDIO")
     assert dic["max_len_seconds"] == "60"
 
 
