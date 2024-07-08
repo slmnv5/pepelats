c778664324550a6a8e65643d615be0a4e574c111
slmnv5 <slmnv5@gmail.com>
2024-07-08 17:10:34 -0400

    1

--- src/mvc/pyscreen.py
+++ src/mvc/pyscreen.py
@@ -21,10 +21,9 @@ _BLUE = 34
 _MAGENTA = 35
 _CYAN = 36
 _WHITE = 37
-_DEFAULT = 39
 
 
-def get_color_str(fg_color_id: int, bk_color_id: int) -> str:
+def _get_color_str(fg_color_id: int, bk_color_id: int) -> str:
     if fg_color_id and not bk_color_id:
         return f"\x1b[{fg_color_id}m"
     elif not fg_color_id and bk_color_id:
@@ -35,13 +34,12 @@ def get_color_str(fg_color_id: int, bk_color_id: int) -> str:
 
 _END_ALL: str = '\x1b[0m'
 _REVERSE_COLOR: str = '\x1b[7m'
-_END_REVERSE: str = '\x1b[27m'
-_RED_COLOR: str = get_color_str(_RED, 0)
-_YELLOW_COLOR: str = get_color_str(_YELLOW, 0)
-_GREEN_COLOR: str = get_color_str(_GREEN, 0)
+_RED_COLOR: str = _get_color_str(_RED, 0)
+_YELLOW_COLOR: str = _get_color_str(_YELLOW, 0)
+_GREEN_COLOR: str = _get_color_str(_GREEN, 0)
 
 
-def get_with_color(line: str, is_rec: bool) -> str:
+def _get_with_color(line: str, is_rec: bool) -> str:
     if not line:
         return ""
     if line[0] == "*":
@@ -64,7 +62,7 @@ class PyScreen(MenuClient):
 
     def _menu_client_redraw(self, draw_info: DrawInfo) -> None:
         self._di = draw_info
-        self._di.header = self._di.header[:SCR_COLS].center(SCR_COLS, '*')
+        self._di.header = self._di.header[:SCR_COLS].center(SCR_COLS)
 
         print(f"\033[1;1H{self._di.header}")  # move cursor to line=1 and pos=1
 
@@ -74,7 +72,7 @@ class PyScreen(MenuClient):
 
         lines = draw_info.content.split('\n')
         for line in lines:
-            line = get_with_color(line.ljust(SCR_COLS), draw_info.is_rec)
+            line = _get_with_color(line.ljust(SCR_COLS), draw_info.is_rec)
             print(line)
         print('\x1b[0J', end='')
 
@@ -87,21 +85,13 @@ class PyScreen(MenuClient):
                 self._di.loop_position = 0.0
 
             line = self._di.header
-            pos2 = 0
             if self._di.max_loop_factor > 1:
                 self._di.max_loop_position += 1.0 / _UPDATES_PER_LOOP / self._di.max_loop_factor
                 if self._di.max_loop_position >= 1:
                     self._di.max_loop_position = 0
-                pos2 = round(self._di.max_loop_position * SCR_COLS)  # shown by blue color
+                pos = round(self._di.max_loop_position * SCR_COLS)
+                line = line[:pos] + '▒' + line[pos + 1:]
 
-            pos1 = round(self._di.loop_position * SCR_COLS)  # shown by inverse background
-            if pos2 > pos1:
-                line = (get_color_str(_YELLOW, _WHITE) + line[:pos1] +
-                        get_color_str(_YELLOW, _BLACK) + line[pos1:pos2] +
-                        _END_ALL + line[pos2:])
-            else:
-                line = (get_color_str(_YELLOW, _WHITE) + line[:pos2] +
-                        get_color_str(_BLACK, _WHITE) + line[pos2:pos1] +
-                        _END_ALL + line[pos1:])
-
+            pos = round(self._di.loop_position * SCR_COLS)  # shown by inverse background
+            line = _REVERSE_COLOR + line[pos:] + _END_ALL + line[pos]
             print(f"\033[1;1H{line}", end='', flush=True)
