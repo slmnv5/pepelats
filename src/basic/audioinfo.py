ff8440c47024bfb6848071c529422f5d865e373d
slmnv5 <slmnv5@gmail.com>
2024-07-10 03:53:57 -0400

    1

--- src/basic/audioinfo.py
+++ src/basic/audioinfo.py
@@ -120,7 +120,7 @@ class AudioInfo:
             sd.check_output_settings(channels=self.SD_CH, dtype=self.SD_TYPE, samplerate=self.SD_RATE)
             sd.check_input_settings(channels=self.SD_CH, dtype=self.SD_TYPE, samplerate=self.SD_RATE)
         except Exception:
-            raise RuntimeError(f"Wrong audio settings for:\n{self.DEV_IN}\n{self.DEV_OUT}")
+            raise RuntimeError("Wrong audio settings")
 
         MyLog().warning(f"=========== Created AudioInfo in process id: {os.getpid()} ==========")
 
