2382e896fdc1997fd3f0674c9c536d262d5f2aac
slmnv5 <slmnv5@gmail.com>
2024-07-15 17:06:16 -0400

    1

--- src/serv/webserver.py
+++ src/serv/webserver.py
@@ -50,7 +50,7 @@ class MyHandler(BaseHTTPRequestHandler):
         self.send_header('Location', '/')  # This will navigate to the original page
         self.end_headers()
 
-    def _send_file(self, fname: str, send_form: bool) -> None:
+    def _send_file(self, fname: str, read_only: bool) -> None:
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
@@ -61,86 +61,88 @@ class MyHandler(BaseHTTPRequestHandler):
         else:
             with open(fname, 'r') as f:
                 data = f.read()
-            if send_form:
-                html = WebHelper.file_form.format(file_name=fname, file_data=data)
-            else:
-                html = "<html><body>" + data + "</body></html>"
 
-            self.wfile.write(html.encode('utf-8'))
+        disabled = "disabled" if read_only else ""
+        html = WebHelper.file_form.format(disabled=disabled, file_name=fname, file_data=data)
+        self.wfile.write(html.encode('utf-8'))
 
-    def _send_config_page(self) -> None:
+
+def _send_config_page(self) -> None:
+    self.send_response(200)
+    self.send_header('Content-type', 'text/html')
+    self.end_headers()
+    self.wfile.write(WebHelper.config_page)
+
+
+# noinspection PyPep8Naming
+def do_GET(self):
+    if self.path == "/":
+        self._send_config_page()
+    elif self.path == _RESET_PATH:
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
-        self.wfile.write(WebHelper.config_page)
+        result = subprocess.run(['git', 'reset', '--hard'], stdout=subprocess.PIPE)
+        self.wfile.write(result.stdout)
+    elif self.path == _EXIT_PATH:
+        raise KeyboardInterrupt()
+    elif self.path.startswith(_EDIT_PATH):
+        fname = self.path[len(_EDIT_PATH):]
+        self._send_file(fname, False)
+    elif self.path.startswith(_SHOW_PATH):
+        fname = self.path[len(_SHOW_PATH):]
+        self._send_file(fname, True)
+    elif self.path == "/favicon.ico":
+        self._send_binary("./favicon.ico")
+    else:
+        self._send_redirect()
 
-    # noinspection PyPep8Naming
-    def do_GET(self):
-        if self.path == "/":
-            self._send_config_page()
-        elif self.path == _RESET_PATH:
+
+# noinspection PyPep8Naming
+def do_POST(self):
+    if self.path != "/save_file":
+        self._send_redirect()
+    else:
+        if self._write_to_file():
             self.send_response(200)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
-            result = subprocess.run(['git', 'reset', '--hard'], stdout=subprocess.PIPE)
-            self.wfile.write(result.stdout)
-        elif self.path == _EXIT_PATH:
-            raise KeyboardInterrupt()
-        elif self.path.startswith(_EDIT_PATH):
-            fname = self.path[len(_EDIT_PATH):]
-            self._send_file(fname, True)
-        elif self.path.startswith(_SHOW_PATH):
-            fname = self.path[len(_SHOW_PATH):]
-            self._send_file(fname, False)
-        elif self.path == "/favicon.ico":
-            self._send_binary("./favicon.ico")
-        else:
-            self._send_redirect()
+            self.wfile.write("Successfully updated file".encode("utf-8"))
 
-    # noinspection PyPep8Naming
-    def do_POST(self):
-        if self.path != "/save_file":
-            self._send_redirect()
-        else:
-            if self._write_to_file():
-                self.send_response(200)
-                self.send_header('Content-type', 'text/html')
-                self.end_headers()
-                self.wfile.write("Successfully updated file".encode("utf-8"))
 
-    def _write_to_file(self) -> bool:
-        content_length = int(self.headers['Content-Length'])
-        post_data = self.rfile.read(content_length).decode("utf-8")  # <--- Gets the data
-        type_list = self.headers.get('Content-Type', '').split("boundary=")
+def _write_to_file(self) -> bool:
+    content_length = int(self.headers['Content-Length'])
+    post_data = self.rfile.read(content_length).decode("utf-8")  # <--- Gets the data
+    type_list = self.headers.get('Content-Type', '').split("boundary=")
 
-        if len(type_list) != 2:
-            self.send_error(401, "Bad Request", "Parsing form data failed")
+    if len(type_list) != 2:
+        self.send_error(401, "Bad Request", "Parsing form data failed")
+        return False
+
+    bound = "--" + type_list[1]  # separates each data item in post_data: k=v
+    mark1 = 'Content-Disposition: form-data; name='  # placed before k=
+    mark2 = '\r\n\r\n'  # placed after k=
+    data_dict = split_to_dict(post_data, bound, mark1, mark2, '\'\" \r\n\t', ' \r\n\t')
+
+    if "file_name" not in data_dict or "file_data" not in data_dict:
+        self.send_error(402, "Bad Request", "Parsing form data failed")
+        return False
+
+    fname = data_dict["file_name"]
+    if not os.path.isfile(fname) or os.path.islink(fname):
+        self.send_error(403, "Bad Request", "Parsing form data failed")
+        return False
+    if fname[-4:].lower() == ".ini":
+        cfg = ConfigParser()
+        try:
+            cfg.read_string(data_dict["file_data"])
+        except ParsingError as ex:
+            self.send_error(405, "Bad Request", f"Parsing form data failed: {ex}")
             return False
 
-        bound = "--" + type_list[1]  # separates each data item in post_data: k=v
-        mark1 = 'Content-Disposition: form-data; name='  # placed before k=
-        mark2 = '\r\n\r\n'  # placed after k=
-        data_dict = split_to_dict(post_data, bound, mark1, mark2, '\'\" \r\n\t', ' \r\n\t')
-
-        if "file_name" not in data_dict or "file_data" not in data_dict:
-            self.send_error(402, "Bad Request", "Parsing form data failed")
-            return False
-
-        fname = data_dict["file_name"]
-        if not os.path.isfile(fname) or os.path.islink(fname):
-            self.send_error(403, "Bad Request", "Parsing form data failed")
-            return False
-        if fname[-4:].lower() == ".ini":
-            cfg = ConfigParser()
-            try:
-                cfg.read_string(data_dict["file_data"])
-            except ParsingError as ex:
-                self.send_error(405, "Bad Request", f"Parsing form data failed: {ex}")
-                return False
-
-        with open(fname, 'w') as f:
-            f.write(data_dict["file_data"])
-        return True
+    with open(fname, 'w') as f:
+        f.write(data_dict["file_data"])
+    return True
 
 
 class WebHelper:
