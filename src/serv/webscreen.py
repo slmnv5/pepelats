e439e3add97f48f7407a5a257395500aa92c79aa
slmnv5 <slmnv5@gmail.com>
2024-07-17 01:41:28 -0400

    1

--- src/serv/webscreen.py
+++ src/serv/webscreen.py
@@ -8,14 +8,6 @@ from serv.webhandler import WebHandler
 from utils.utilconfig import IP_ADDR
 
 
-_END_ALL: str = '</span>'
-_REV_CLR: str = '<span id="rev_span" style="red"'
-_RED_CLR: str = '<span id="red_span" style="color: red;">'
-_YEL_CLR: str = f"\x1b[1;{_YELLOW}m"
-_GRN_CLR: str = f"\x1b[1;{_GREEN}m"
-_BLU_CLR: str = f"\x1b[1;{_BLUE}m"
-
-
 class WebScreen(MenuClient, HTTPServer):
     def __init__(self, queue: Queue):
         MenuClient.__init__(self, queue)
@@ -29,17 +21,6 @@ class WebScreen(MenuClient, HTTPServer):
     def _client_stop(self) -> None:
         super()._client_stop()
         self.shutdown()
-
-    def __add_color(self, line: str) -> str:
-        if line[0] == "*":
-            if self._di.is_rec:
-                return _RED_CLR + line + _END_ALL
-            else:
-                return _GRN_CLR + line + _END_ALL
-        elif line[0] == "~":
-            return _YEL_CLR + line + _END_ALL
-        else:
-            return line
 
     def _client_redraw(self, di: DrawInfo) -> None:
         self._di = di
