8d8ec6c1f3c1250875bf7d774d2d34882fd660bb
slmnv5 <slmnv5@gmail.com>
2024-07-26 19:05:21 -0400

    1

--- test/test_menuhost.py
+++ test/test_menuhost.py
@@ -3,7 +3,7 @@ from utils.util_config import convert_param
 
 
 def test_1():
-    assert convert_param('lkj') == 'lkj'
+    assert convert_param(' l k j ') == ' l k j '
     assert convert_param(' +123  ') == 123
     assert convert_param(' -123.99  ') == -123.99
-    assert convert_param(' -123.99. ') != -123.99
+    assert convert_param(' -123.99. ') == ' -123.99. '
