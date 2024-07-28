79f8caa9d2bd77b8fdb4d9dec2da89e426dd1603
slmnv5 <slmnv5@gmail.com>
2024-07-28 03:47:16 -0400

    1

--- src/screen/webhandler.py
+++ src/screen/webhandler.py
@@ -1,16 +1,39 @@
 from http.server import BaseHTTPRequestHandler
+from threading import Event
 
 from utils.util_web import send_headers, UPDATE_PAGE_B, FAVICON_B, UPDATE_CODE_B
 
 
 class WebHandler(BaseHTTPRequestHandler):
+    has_updates: Event = Event()
+    updates_b: bytes = b""
+    waiting_clients: set = set()
+
+    def _process_update(self) -> None:
+        client = self.address_string()
+        if client not in WebHandler.waiting_clients:
+            WebHandler.waiting_clients.add(client)
+            WebHandler.has_updates.wait()
+            WebHandler.waiting_clients.remove(client)
+        else:
+            self.wfile.write(WebHandler.updates_b)  # if same client already waiting send past data
+
+        if not WebHandler.waiting_clients:
+            WebHandler.has_updates.clear()
+
     # noinspection PyPep8Naming
     def do_GET(self):
         if self.path == "/update":
             send_headers(self, 'application/json')
-            # noinspection PyUnresolvedReferences
-            updates: bytes = self.server.get_updates()
-            self.wfile.write(updates)
+            client = self.address_string()
+            if client not in WebHandler.waiting_clients:
+                WebHandler.waiting_clients.add(client)
+                WebHandler.has_updates.wait()
+                WebHandler.waiting_clients.remove(client)
+
+            self.wfile.write(WebHandler.updates_b)  # if same client already waiting send past data
+            if not WebHandler.waiting_clients:
+                WebHandler.has_updates.clear()
         elif self.path == "/update_page.js":
             send_headers(self, 'text/javascript')
             self.wfile.write(UPDATE_CODE_B)
--- src/screen/webscreen.py
+++ src/screen/webscreen.py
@@ -1,6 +1,6 @@
 import json
 from http.server import HTTPServer
-from multiprocessing import Queue, Event
+from multiprocessing import Queue
 from threading import Thread
 
 from screen.menuclient import MenuClient
@@ -14,10 +14,8 @@ class WebScreen(MenuClient, HTTPServer):
     def __init__(self, queue: Queue):
         MenuClient.__init__(self, queue)
         # noinspection PyTypeChecker
-        HTTPServer.__init__(self, ("", LOCAL_PORT), WebHandler)
+        HTTPServer.__init__(self, ("", LOCAL_PORT), WebHandler, )
         self.__dic: dict = get_screen_dict(get_default_dict())
-        self._has_updates: Event = Event()
-        self._updates_b: bytes = b" "
         Thread(target=self.__update, name="update", daemon=True).start()
 
     def __update(self) -> None:
@@ -29,8 +27,8 @@ class WebScreen(MenuClient, HTTPServer):
 
     def _client_log(self, msg: str) -> None:
         self.__dic["description"] = msg
-        self._updates_b = json.dumps(self.__dic).encode()
-        self._has_updates.set()
+        WebHandler.updates_b = json.dumps(self.__dic).encode()
+        WebHandler.has_updates.set()
 
     def service_actions(self):
         if not self._alive:
@@ -38,13 +36,8 @@ class WebScreen(MenuClient, HTTPServer):
 
     def _client_redraw(self, dic: dict) -> None:
         self.__dic = get_screen_dict(dic)
-        self._updates_b = json.dumps(self.__dic).encode()
-        self._has_updates.set()
-
-    def get_updates(self) -> bytes:
-        self._has_updates.wait()
-        self._has_updates.clear()
-        return self._updates_b
+        WebHandler.updates_b = json.dumps(self.__dic).encode()
+        WebHandler.has_updates.set()
 
 
 if __name__ == "__main__":
