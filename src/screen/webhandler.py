7aabce6f7e6fdcaffd9faf0b5ced3e0908d8e1bc
slmnv5 <slmnv5@gmail.com>
2024-07-29 00:53:51 -0400

    1

--- src/screen/webhandler.py
+++ src/screen/webhandler.py
@@ -5,16 +5,20 @@ from utils.util_web import send_headers, FAVICON_B, UPDATE_CODE_B, UPDATE_PAGE
 
 class WebHandler(BaseHTTPRequestHandler):
     def __init__(self, update_b: bytes, *args, **kwargs):
-        self.update_b = update_b
+        self._update_b = update_b
+        print(11111111111, self._update_b)
         # BaseHTTPRequestHandler calls do_GET inside __init__
         # So we have to call super().__init__ after setting attributes.
         super().__init__(*args, **kwargs)
 
     # noinspection PyPep8Naming
     def do_GET(self):
-        if self.path == "/update":
+        if not self._update_b:
+            self.send_error(500, "server stopped")
+            raise KeyboardInterrupt("server stopped")
+        elif self.path == "/update":
             send_headers(self, 'application/json')
-            self.wfile.write(self.update_b)
+            self.wfile.write(self._update_b)
         elif self.path == "/update_page.js":
             send_headers(self, 'text/javascript')
             self.wfile.write(UPDATE_CODE_B)
--- src/screen/webscreen.py
+++ src/screen/webscreen.py
@@ -11,27 +11,24 @@ from utils.util_log import MY_LOG
 from utils.util_screen import get_default_dict
 
 
-class WebScreen(MenuClient, HTTPServer):
+class WebScreen(MenuClient):
     def __init__(self, queue: Queue):
         MenuClient.__init__(self, queue)
-        self.__dic: dict = dict()
-        self._updates_b: bytes = b""
+        self.__dic: dict = get_default_dict()
+        self.__dic["update_tm"] = time()
+        self._updates_b: bytes = json.dumps(self.__dic).encode()
         handler_class = partial(WebHandler, self._updates_b)
-        HTTPServer.__init__(self, ("", LOCAL_PORT), handler_class)
+        self._serv = HTTPServer(("", LOCAL_PORT), handler_class)
         self._client_redraw(get_default_dict())
         MY_LOG.warning(f"To control looper connect to:\nhttp://{LOCAL_IP}:{LOCAL_PORT}")
         try:
-            self.serve_forever()
+            self._serv.serve_forever()
         except KeyboardInterrupt:
-            self.server_close()
+            self._serv.server_close()
 
     def _client_log(self, msg: str) -> None:
         self.__dic["description"] = msg
         self._updates_b = json.dumps(self.__dic).encode()
-
-    def service_actions(self):
-        if not self._alive:
-            raise KeyboardInterrupt("stopped")
 
     def _client_redraw(self, dic: dict) -> None:
         self.__dic = dic
