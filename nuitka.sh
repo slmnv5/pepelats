d47af34f789c4e60763822501bfad83b971cbe0b
slmnv5 <slmnv5@gmail.com>
2025-02-20 21:12:25 -0500

    1

--- nuitka.sh
+++ nuitka.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-change_boot() {
-  sed -i 's|nuitka.sh|looper.sh|g' ~/.bashrc
-  sudo reboot
-}
-
 if pidof -o %PPID -x "nuitka.sh">/dev/null; then
     echo "Process already running"
     exit 1
@@ -18,19 +13,6 @@ pkill -9 looper.sh
 D1="$HOME/musiloop"
 D2="$HOME/pepelats"
 EXE="$D2/main.dist/main.bin"
-
-cd "$D1" || exit 1
-git reset --hard
-if [ -f "$EXE" ]; then
-  isOK=$(git pull | grep "Already up to date")
-  if [ -n "$isOK" ]; then change_boot; fi
-fi
-
-cd "$D2" || exit 1
-git reset --hard
-git pull
-
-sleep 5
 
 cd "$D1" || exit 1
 python -m nuitka --standalone --output-dir="$D2/" --python-flag=-OO ./src/main.py
@@ -49,5 +31,4 @@ cd "$D2" || exit 1
 git add --all
 git commit -m"auto commit new build"
 git push
-change_boot
 
