8b2d1d5164bac71a48aa062ba4024ccf49f95b6b
slmnv5 <slmnv5@gmail.com>
2025-02-20 21:10:57 -0500

    1

--- looper.sh
+++ looper.sh
@@ -2,35 +2,26 @@
 
 # sudo required for keyboard
 SUDO=""
-export HAS_KBD=""
 
-change_boot() {
-  sed -i 's|looper.sh|nuitka.sh|g' ~/.bashrc
-  sudo reboot
-}
-
-test_all() {
-  # sudo required for keyboard
-  SUDO=""
-  HAS_KBD=""
-  if lsusb | grep -i "USB Keyboard"; then HAS_KBD="YES"; SUDO="sudo -E"; fi
-}
-
-TMP=$(dirname "$0")
-cd "$TMP" || exit 1
-
-if pidof -o %PPID -x "looper.sh">/dev/null; then
+ if pidof -o %PPID -x "$(basename "$0")">/dev/null; then
     echo "Process already running"
     exit 1
 fi
+cd "$(dirname "$0")" || exit 1
 
-mkdir ~/save_song
-touch local.ini log.txt
-chmod a+rw log.txt log.bak ~/save_song/*
+mkdir ~/save_song 2>/dev/null
+touch local.ini log.txt log.bak
+chmod a+rw log.txt log.bak ~/save_song/* 2>/dev/null
 cat log.txt >> log.bak
 tail -n 1000 log.bak > log.txt
 mv log.txt log.bak
 echo "== $(date) ==" > log.txt
+
+for var in "$@"; do
+  if [ "$var" = "--kbd" ] || [ "$var" = "--keyboard" ]; then
+    SUDO="sudo -E"
+  fi
+done
 
 # disable under voltage error on screen, set huge font size, disable typing echo
 sudo dmesg -D
@@ -38,7 +29,6 @@ sudo setfont Uni1-VGA32x16
 #stty -echo
 for count in 1 2 3 4 5 6 7 8 9; do
   echo "run # $count"
-  test_all
   killall -9 -qw python > /dev/null
   PYTHON_CMD="$SUDO ./main.dist/main.bin $*"
   $PYTHON_CMD
