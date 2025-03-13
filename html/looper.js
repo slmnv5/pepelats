17c7266bc5a623bcb644553f50db9241d81b83cf
pi <pi@pepe3.com>
2025-03-13 14:35:23 +0000

    auto commit new build

--- html/looper.js
+++ html/looper.js
@@ -3,33 +3,26 @@
 let WIN_CHARS = 30; // how many chars fit in browser window line
 // const TEXT_SZ = 35; // size for all elements
 const BW_S = '<span style="color: black; background-color: rgb(200, 200, 200)";>'; // 1-st loop position
-const WY_S = '<span style="background-color: yellow";>';  // max. length loop position
 const END_S = '</span>'
+
 const RED_P = '<p style="color: rgb(250, 30, 30);">';
 const GREEN_P = '<p style="color: rgb(30, 250, 30);">';
 const YELLOW_P = '<p style="color: yellow;">';
 
 
-function sleepFunc(ms) {
-    if (!Number.isFinite(ms) || ms < 0) ms = 1_000;
-    return new Promise((r) => {setTimeout(r, ms)});
-}
-
+function getHeaderStr (s) {
+    const missing_len = Math.max(WIN_CHARS - s.length, 0);
+    return '.'.repeat(missing_len / 2) + s + '.'.repeat(missing_len / 2);
+};
 
 // make html showing progress line - position in 1-st loop and position in max. length loop
 function getHeaderHtml (s, base_pos, long_pos) {
-    let missing_len = Math.max(WIN_CHARS - s.length, 0);
-    s = '.'.repeat(missing_len / 2) + s + '.'.repeat(missing_len / 2);
-
     function decorateOneChar(s, pos) {
-        if (pos < 0 || pos >= s.length) return s;
         return s.slice(0, pos) + "*" + s.slice(pos+1);
     }
-    let s1, s2, l1, l2;
-    [l1, l2] = [s.length * base_pos, s.length * long_pos];
-    [s1, s2] = [s.slice(0, l1), s.slice(l1)];
-    [s1, s2] = [decorateOneChar(s1, l2), decorateOneChar(s2, l2 - l1)];
-    return '<p>' + BW_S + s1 + END_S + s2 + '</p>';
+    const l = s.length;
+    s = decorateOneChar(s, l * long_pos);
+    return '<p>' + BW_S + s.slice(0, l * base_pos) + END_S + s.slice(l * base_pos) + '</p>';
 };
 
 // decorate string with color based on 1st char. 
@@ -45,61 +38,52 @@ function getContentHtml(content, is_rec) {
         } else {
             result += '<p>' + s + '</p>';
         }
-    }
+    };
     return result;
 };
 
-
-
 window.onload = () => {
-    let DATA = {"sleep_tm":1, "header":"-", "description": "-", "content":"-", "is_rec":0,
-            "base_pos":0, "long_pos":0, "base_delta":0.0625, "long_delta":0.0625};
-    let KEEP_RUNNING = true;
 
-    const URL = '/update';
     const HEADER = document.getElementById('header');
     const DESCRIPTION = document.getElementById('description');
     const CONTENT = document.getElementById('content');
+    const UPD_REQUEST = new XMLHttpRequest();
 
-    
-    Promise.race([fetchData(), redrawData()])
-    .then((x) => console.log("Resolved:", x))  
-    .catch((x) => console.log("Rejected:", x))
+    let TIMEOUT = 0;
 
-
-    async function fetchData() {
-        while(KEEP_RUNNING) {
-            try {
-                let resp = await fetch(URL);
-                if (resp.status == 200) {
-                    DATA = await resp.json();
-                    DESCRIPTION.textContent = DATA.description;
-                    CONTENT.innerHTML = getContentHtml(DATA.content, DATA.is_rec);
-                } else if (resp.status >= 300) {
-                    throw "Server error: " + resp.status;
-                }
-            } catch(err) {
-                DESCRIPTION.textContent = err.message;
-                KEEP_RUNNING = false;
-            }
-        };
+    function fetchData() {
+        UPD_REQUEST.open("GET", '/update', true);
+        UPD_REQUEST.send();
     };
 
-    async function redrawData() {
-        while(KEEP_RUNNING) {
-            try {
-                await sleepFunc(DATA.sleep_tm * 1000);
-                DATA.base_pos += DATA.base_delta // position in the 1-st loop
-                DATA.base_pos %= 1;
-                DATA.long_pos += DATA.long_delta // position in the max. length loop
-                DATA.long_pos %= 1;
-                HEADER.innerHTML = getHeaderHtml(DATA.header, DATA.base_pos, DATA.long_pos);
-            } catch(err) {
-                DESCRIPTION.textContent = err.message;
-                KEEP_RUNNING = false;
-            }
-        };
+    UPD_REQUEST.onloadend = () => {
+        if (UPD_REQUEST.status == 204) {
+            fetchData(); // no update, repeat again;
+        } else if (UPD_REQUEST.status == 200) {
+            const data = JSON.parse(UPD_REQUEST.responseText);
+            DESCRIPTION.textContent = data.description;
+            CONTENT.innerHTML = getContentHtml(data.content, data.is_rec);
+            data.header = getHeaderStr(data.header);
+            clearTimeout(TIMEOUT);
+            redrawData(data);
+            fetchData(data); // has updated, repeat again;
+        } else {
+            DESCRIPTION.textContent = "Incorrect server response: " + UPD_REQUEST.status;
+            clearTimeout(TIMEOUT);
+         };
     };
+
+    function redrawData(data) {
+        data.base_pos += data.base_delta // position in the 1-st loop
+        data.base_pos %= 1;
+        data.long_pos += data.long_delta // position in the max. length loop
+        data.long_pos %= 1;
+        HEADER.innerHTML = getHeaderHtml(data.header, data.base_pos, data.long_pos);
+        TIMEOUT = setTimeout(()=>{redrawData(data)}, data.sleep_tm * 1000);
+    };
+
+    // only one call at the end
+    fetchData();
+
+
 };
-
-
